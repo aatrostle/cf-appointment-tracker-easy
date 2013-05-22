18f0edeaebf7e9cf454ba9378e0b0c08eac8d096
Aaron Trostle <aaron.trostle@gmail.com>
2013-05-22 15:05:37 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppointmentTrackerEasy::Application.config.secret_token = 'a3502dda065c79e7e0f4d357ec09d5d81920bd3456b6b33caa1615935f504f9a04b16ef65c6106c9c0285f741f855309b959384bb19af26a4d34ab0867025331'
