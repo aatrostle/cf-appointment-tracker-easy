7747e0bdc549cef79d3da0a6c757069ec46db603
Aaron Trostle <aaron.trostle@gmail.com>
2013-05-22 22:18:11 -0700

    Enable controller actions to exercise has_many through relationship of Appointments, Applicants and Instructors.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,83 +1,43 @@
 class AppointmentsController < ApplicationController
-  # GET /appointments
-  # GET /appointments.json
+  before_filter :find_appointment, only: [:show, :edit, :update, :destroy]
+
   def index
     @appointments = Appointment.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @appointments }
-    end
   end
 
-  # GET /appointments/1
-  # GET /appointments/1.json
-  def show
-    @appointment = Appointment.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @appointment }
-    end
-  end
-
-  # GET /appointments/new
-  # GET /appointments/new.json
   def new
     @appointment = Appointment.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @appointment }
-    end
   end
 
-  # GET /appointments/1/edit
-  def edit
-    @appointment = Appointment.find(params[:id])
-  end
+  # def edit
+  # end
 
-  # POST /appointments
-  # POST /appointments.json
   def create
     @appointment = Appointment.new(params[:appointment])
-
-    respond_to do |format|
-      if @appointment.save
-        format.html { redirect_to @appointment, notice: 'Appointment was successfully created.' }
-        format.json { render json: @appointment, status: :created, location: @appointment }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @appointment.errors, status: :unprocessable_entity }
-      end
+    if @appointment.save
+      redirect_to '/', notice: "Appointment has been scheduled."
+    else
+      render action: "new", alert: "Appointment was not scheduled!"
     end
   end
 
-  # PUT /appointments/1
-  # PUT /appointments/1.json
   def update
-    @appointment = Appointment.find(params[:id])
-
-    respond_to do |format|
-      if @appointment.update_attributes(params[:appointment])
-        format.html { redirect_to @appointment, notice: 'Appointment was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @appointment.errors, status: :unprocessable_entity }
-      end
+    if @appointment.update_attributes(params[:appointment])
+      redirect_to '/', notice: "Appointment updated!"
+    else
+      render action: "edit", alert: "Appointment was not updated."
     end
   end
 
-  # DELETE /appointments/1
-  # DELETE /appointments/1.json
   def destroy
-    @appointment = Appointment.find(params[:id])
     @appointment.destroy
+    redirect_to '/', notice: "Appointment cancelled!"
+  end
 
-    respond_to do |format|
-      format.html { redirect_to appointments_url }
-      format.json { head :no_content }
-    end
+  private
+
+  def find_appointment
+    @appointment = Appointment.find(params[:id])
   end
 end
--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,84 +1,25 @@
-
 class InstructorsController < ApplicationController
-  # GET /instructors
-  # GET /instructors.json
-  def index
-    @instructors = Instructor.all
+  before_filter :find_instructor, only: [:list_app, :show, :edit, :update, :destroy]
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @instructors }
-    end
-  end
-
-  # GET /instructors/1
-  # GET /instructors/1.json
-  def show
-    @instructor = Instructor.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @instructor }
-    end
-  end
-
-  # GET /instructors/new
-  # GET /instructors/new.json
   def new
     @instructor = Instructor.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @instructor }
-    end
   end
 
-  # GET /instructors/1/edit
-  def edit
-    @instructor = Instructor.find(params[:id])
-  end
-
-  # POST /instructors
-  # POST /instructors.json
   def create
-    @instructor = Instructor.new(params[:instructor])
-
-    respond_to do |format|
-      if @instructor.save
-        format.html { redirect_to @instructor, notice: 'Instructor was successfully created.' }
-        format.json { render json: @instructor, status: :created, location: @instructor }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @instructor.errors, status: :unprocessable_entity }
-      end
+    @instructor = Instructor.create(params[:instructor])
+    if @instructor.save
+      redirect_to '/', notice: "Instructor created."
+    else
+      render action: "new", notice: "Instructor was not created."
     end
   end
 
-  # PUT /instructors/1
-  # PUT /instructors/1.json
-  def update
-    @instructor = Instructor.find(params[:id])
+  def list_app
 
-    respond_to do |format|
-      if @instructor.update_attributes(params[:instructor])
-        format.html { redirect_to @instructor, notice: 'Instructor was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @instructor.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
-  # DELETE /instructors/1
-  # DELETE /instructors/1.json
-  def destroy
-    @instructor = Instructor.find(params[:id])
-    @instructor.destroy
-
-    respond_to do |format|
-      format.html { redirect_to instructors_url }
-      format.json { head :no_content }
+  private
+    def find_instructor
+      @instructor = Instructor.find(params[:id])
     end
-  end
 end
